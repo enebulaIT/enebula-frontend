9b85c5c8031f6c7abc79dc0452f1a2094b934ee7
Anirudha sah <anirudha.s@simformsolutions.com>
2020-10-28 01:47:37 +0530

    changes

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -22,7 +22,7 @@ const About = props => {
 							<div className="col-lg-6">
 								<div className="hero__standout--headline">
 									<h2 className="section__heading">About Us </h2>
-									<blockquote>A team away from your team.</blockquote>
+									<blockquote>An extension to your team..</blockquote>
 									<i>- eNebula</i>
 								</div>
 							</div>
@@ -30,7 +30,7 @@ const About = props => {
 								<div className="hero__standout--content">
 									<p>We continuously strive to build digital solutions that would help your business thrive in this economy which is turning to a digital one at godspeed. We believe that not so far in the future digital presence will not just be an added advantage for a business but will be an added building block for any business to get started/continue to exist.</p>
 
-									<p>You might ask. Why us? What makes us qualified to handle such an important aspect of a business? Great question! We are a team of highly seasoned and passionate individuals ready to help you build and get you going on your path of success. <b>We are an extension to your team.</b> With us you’ll be getting in touch with some world class leaders who’re offering world-class digital services to world-class people who’re building world-class solutions. Guess that’s enough world-class talk from the apprentice whose world is his class. 😉</p>
+									<p>You might ask. Why us? What makes us qualified to handle such an important aspect of a business? Great question! We are a team of highly seasoned and passionate individuals ready to help you build and get you going on your path of success. With us you’ll be getting in touch with some world class leaders who’re offering world-class digital services to world-class people who’re building world-class solutions. Guess that’s enough world-class talk from the apprentice whose world is his class. 😉</p>
 
 									<Link to = "/contact" className="btn btn-primary btn-rounded btn-stroke">Get in touch with us </Link>
 								</div>
