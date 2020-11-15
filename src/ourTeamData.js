5f06c1ff900e6c6f9b4e3a951f0f7c9b1f218735
Anirudha sah <anirudha.s@simformsolutions.com>
2020-11-15 23:00:47 +0530

    changes

--- src/ourTeamData.js
+++ src/ourTeamData.js
@@ -9,49 +9,34 @@ export default [
     {
         featured_image: AnirudhImage,
         post_title: "Anirudha Sah",
-        designation: "Web Developer",
-        facebook_link: "https://facebook.com",
-        twitter_link: "https://twitter.com",
-        linkedin_link: "https://linkedin.com" 
+        designation: "Lead Frontend Developer", 
+        twitter_link: "https://twitter.com/anna007sah",
+        linkedin_link: "https://www.linkedin.com/in/anirudha-sah-0b966b146/" 
     },
     {
         featured_image: AshitImage,
         post_title: "Ashit Hingrajiya",
-        designation: "Web Developer",
-        facebook_link: "https://facebook.com",
-        twitter_link: "https://twitter.com",
-        linkedin_link: "https://linkedin.com" 
-    },
-    {
-        featured_image: JemsImage,
-        post_title: "Jemish Dhameliya",
-        designation: "Mobile App Developer",
-        facebook_link: "https://facebook.com",
-        twitter_link: "https://twitter.com",
+        designation: "Lead Backend Developer",  
+        twitter_link: "https://twitter.com/attaboy_ashit",
         linkedin_link: "https://linkedin.com" 
     },
     {
         featured_image: MayurImage,
         post_title: "Mayurdhwajsinh Gohil",
-        designation: "Mobile App Developer",
-        facebook_link: "https://facebook.com",
-        twitter_link: "https://twitter.com",
-        linkedin_link: "https://linkedin.com" 
+        designation: "Lead App Developer", 
+        twitter_link: "https://twitter.com/mayur_g7",
+        linkedin_link: "https://www.linkedin.com/in/ashit-hingrajiya-303857154/" 
     },
     {
         featured_image: JunedImage,
         post_title: "Juned Theba",
-        designation: "UI/UX developer",
-        facebook_link: "https://facebook.com",
-        twitter_link: "https://twitter.com",
-        linkedin_link: "https://linkedin.com" 
+        designation: "Lead UI/UX developer", 
+        twitter_link: "https://twitter.com/juned_theba",
+        linkedin_link: "https://www.linkedin.com/in/juned-theba-a71630127/" 
     },
     {
         featured_image: VatsalImage,
         post_title: "Vatsal Raichura",
-        designation: "Cyber Security Consultant",
-        facebook_link: "https://facebook.com",
-        twitter_link: "https://twitter.com",
-        linkedin_link: "https://linkedin.com" 
+        designation: "Lead Cyber Security Consultant",  
     },
 ];
