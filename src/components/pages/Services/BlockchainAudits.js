96e2dc39df875a6cf5c098c0be47441018daa963
Anirudha sah <anirudha.s@simformsolutions.com>
2021-07-09 22:17:21 +0530

    audit-report files api integration

--- src/components/pages/Services/BlockchainAudits.js
+++ src/components/pages/Services/BlockchainAudits.js
@@ -1,15 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { Table } from 'antd';
+import axios from 'axios';
 import downloadButton from '../../../images/download-icon.svg';
-import dataSource from '../../../data/blockchainAudits/blockChainAudits';
 
 const BlockchainAudits = (props) => {
+  const [auditData, setAuditData] = useState([]);
+  const [dataLoading, setDataLoading] = useState(true);
+
   useEffect(() => {
     setTimeout(() => {
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }, 100);
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      'https://t9s9v4aan6.execute-api.ap-south-1.amazonaws.com/default/getAllAuditData'
+    );
+    // Error case
+    if (response.status !== 200) {
+      setAuditData([]);
+    }
+
+    // Positive case
+    let parsedData = [];
+    response.data.forEach((audiData) => {
+      if (!audiData.IsDeleted) {
+        parsedData.push({
+          key: audiData._id,
+          name: audiData.Name,
+          coverage: audiData.Coverage,
+          type: audiData.Type,
+          language: audiData.Language,
+          contractType: audiData.ContractType,
+          stages: audiData.Stages,
+          filePath: audiData.DocLink,
+        });
+      }
+    });
+    setAuditData([...parsedData]);
+    setDataLoading(false);
+  };
 
   const columns = [
     {
@@ -61,13 +94,19 @@ const BlockchainAudits = (props) => {
     },
   ];
 
+  console.log(auditData);
   return (
     <div>
       <section className="section pt-40 pb-0">
         <div className="container">
           <br />
           <h1>Reports</h1>
-          <Table dataSource={dataSource} columns={columns} pagination={false} />
+          <Table
+            loading={dataLoading}
+            dataSource={auditData}
+            columns={columns}
+            pagination={false}
+          />
         </div>
       </section>
     </div>
